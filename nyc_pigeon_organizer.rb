2965068e1ce8451ce66efae27abae65854aca654
Iam H <sumaya_khair@live.com>
2020-01-08 17:48:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  newhash = {}
+  collection = [ ]
+  data.each { |key, value| value.each{|att, names| names.each {|name|
+  if !collection.include?(name) 
+  collection << name 
+  end
+  }
+  }
+  }
+ collection.each {|name| newhash[name]}
+ 
 end
